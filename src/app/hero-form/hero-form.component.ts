efc34c0bffc25b479cfff0d00a389a530891243c
Arthur Siqueira <arthur.siq5@gmail.com>
2020-01-27 14:22:59 -0300

    atualizada documentação

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -19,8 +19,16 @@ export class HeroFormComponent{
     'Weather Changer'
   ];
 
+  /**
+   * @var Hero
+   * @description used to submit data
+   */
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
+  /**
+   * @var boolean
+   * @description used to check if the form is submitted or not
+   */
   submitted = false;
 
   onSubmit() { 
